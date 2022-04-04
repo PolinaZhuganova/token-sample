6add52ed9e3f788d783e5655381b67c31b228450
Polik <ppolina93@yandex.ru>
2022-04-04 18:59:17 +0300

    авторицация пользователей на основе прав (authorities)

--- src/main/java/com/example/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/example/springsecuritydemo/config/SecurityConfig.java
@@ -4,6 +4,7 @@
 
 package com.example.springsecuritydemo.config;
 
+import com.example.springsecuritydemo.model.*;
 import com.example.springsecuritydemo.model.Role;
 import org.springframework.context.annotation.*;
 import org.springframework.http.HttpMethod;
@@ -29,9 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.csrf().disable()
 			.authorizeRequests()
 			.antMatchers().permitAll()
-			.antMatchers(HttpMethod.GET, "/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
-			.antMatchers(HttpMethod.POST, "/api/**").hasRole(Role.ADMIN.name())
-			.antMatchers(HttpMethod.DELETE, "/api/**").hasRole(Role.ADMIN.name())
+			.antMatchers(HttpMethod.GET, "/api/**").hasAuthority(Permission.DEVELOPERS_READ.getPermission())
+			.antMatchers(HttpMethod.POST, "/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
+			.antMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
 			.anyRequest()
 			.authenticated()
 			.and()
@@ -46,12 +47,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			User.builder()
 				.username("admin")
 				.password(passwordEncoder().encode("admin"))
-				.roles(Role.ADMIN.name())
+				.authorities(Role.ADMIN.getAuthorities())
 				.build(),
 			User.builder()
 				.username("user")
 				.password(passwordEncoder().encode("user"))
-				.roles(Role.USER.name())
+				.authorities(Role.USER.getAuthorities())
 				.build()
 		);
 	}
